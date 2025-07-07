ef39ec16f180d626142b9da81b3d41f92f1297a4
Camilo <osoriojuancamilo315@gmail.com>
2025-07-06 20:50:12 -0500

    docs: agrega comentarios para la configuración

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+// URLS para API
 export const API_URL = import.meta.env.VITE_CORS_ORIGIN;
 export const IMG_URL = import.meta.env.VITE_IMG_URL;
 
-
+// Configuración base para axios
 export const axiosInstance = axios.create({
     baseURL: API_URL,
     withCredentials: true
