eeedddb310e4734198d9a7a642ac17d1c8dfddd6
Camilo <osoriojuancamilo315@gmail.com>
2025-07-06 20:43:18 -0500

    refactor: se agrega un valor default a las propiedades type y disabled

--- client/src/components/Buttons/ButtonBlank.jsx
+++ client/src/components/Buttons/ButtonBlank.jsx
@@ -1,6 +1,6 @@
 import "../../style/button.css"
 
-export default function ButtonBlank({ color, letterColor, disabled, type, children, onClick }) {
+export default function ButtonBlank({ color, letterColor, disabled = false, type = "button", children, onClick }) {
     return (
         <button onClick={onClick} disabled={disabled} type={type} className={`btn-blank`} style={{ "--color": color, "--letterColor": letterColor }} >{children}</button>
     )
