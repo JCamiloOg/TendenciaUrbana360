75f4ea16428139376c7459ecacf5425d7872c4f8
Camilo <osoriojuancamilo315@gmail.com>
2025-07-11 16:26:53 -0500

    feat: cambia el tipo de carousel a embla-carousel-react para más dinamismo

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,6 +1,7 @@
 // Modules
 import { useParams } from "react-router-dom"
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import useEmblaCarousel from 'embla-carousel-react'
 
 // components
 import Error from "./Error";
@@ -15,25 +16,28 @@ import formatCurrency from "@/utils/formatCurrency";
 import Select from "@/components/Inputs/Select";
 import ButtonBlank from "@/components/Buttons/ButtonBlank";
 import Footer from "@/components/Footer";
+import sizesGuide from "../assets/guiasTallas.jpg"
 
 // Hooks
 import { useModalLogin, useModalRegister } from "@/hooks/useModal";
 import { usePageLoader } from "@/hooks/useLoader";
 import getExtension from "@/utils/getExtension";
-import Autoplay from "embla-carousel-autoplay";
 
 // Services
 import { getProductByID } from "@/services/productsService";
 import { IMG_URL } from "@/config";
+import ModalImage from "@/components/Modals/ModalImage";
+import SelectWithImages from "@/components/Inputs/SelectWithImage";
+import SelectRounded from "@/components/Inputs/SelectRounded";
 
 export default function Product() {
     const { type, id } = useParams();
-    const validCategories = [
-        'calzado', 'camisas', 'pantalones', 'gafas', 'gorras', 'relojes', 'perfumes', 'vapeadores'
-    ];
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false })
+
     const { isOpenModal, isVisible, close, open } = useModalLogin();
     const { isOpenModalRegister, isVisibleReigster, openRegister, closeRegister } = useModalRegister();
     const { loading, startLoading, stopLoading } = usePageLoader();
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
     const [product, setProduct] = useState([]);
     const [description, setDescription] = useState(false);
@@ -43,13 +47,19 @@ export default function Product() {
     const [isLogin, setIsLogin] = useState(null);
     const [error, setError] = useState(null);
     const [info, setInfo] = useState(false);
+    const [isOpenModalImage, setIsOpenModalImage] = useState(false);
+    const [imageModal, setImageModal] = useState("");
+
+    const [productSelected, setProductSelected] = useState(false);
+    const [sizesSelected, setSizesSelected] = useState(false);
+    const [typeSelected, setTypeSelected] = useState(false);
+
 
     const getProduct = async (type, id) => {
         startLoading();
         try {
             const res = await getProductByID(type, id);
             if (res.status === 200) {
-                console.log(res);
                 setSizes(res.data.tallas);
                 setExtra(res.data.extra);
                 setDescription(res.data.descripcion);
@@ -70,8 +80,34 @@ export default function Product() {
 
     const handleType = (val) => {
         const prices = JSON.parse(localStorage.getItem("types"));
-        setPrice(prices[val] ? prices[val] : product.Precio)
+        setPrice(prices[val] ? prices[val] : product.Precio);
+        if (val === typeSelected) setTypeSelected(false);
+        else setTypeSelected(val);
     }
+
+    const handleSizes = (val) => {
+        if (val === sizesSelected) setSizesSelected(false);
+        else setSizesSelected(val);
+    }
+
+    const handleModalImg = (img) => {
+        setIsOpenModalImage(!isOpenModalImage);
+        setImageModal(img);
+    }
+
+    const scrollTo = useCallback((index, id) => {
+        if (emblaApi) emblaApi.scrollTo(index)
+        setSelectedIndex(index)
+        setProductSelected(id)
+    }, [emblaApi])
+
+    useEffect(() => {
+        if (emblaApi) {
+            emblaApi.on('select', () => {
+                setSelectedIndex(emblaApi.selectedScrollSnap())
+            })
+        }
+    }, [emblaApi])
 
     useEffect(() => {
         getProduct(type, id);
@@ -91,7 +127,6 @@ export default function Product() {
 
     }, [product, extra])
 
-    if (!validCategories.includes(type)) return <Error status={404} error={"Página no encontrada"} />
     if (error) return <Error status={error.status} error={error.message} />
     if (info) return <NoInfoComplete />
     return (
@@ -100,41 +135,38 @@ export default function Product() {
             <NavBar openLogin={open} openRegister={openRegister} isLogin={isLogin} />
             <div className="container mx-auto px-10 pt-30">
                 <div className="grid grid-cols-12 bg-white p-14 rounded-2xl border-gray-700">
-                    <div className="col-span-8 place-items-center">
+                    <div className="2xl:col-span-8 xl:col-span-8 lg:col-span-8 md:col-span-8 sm:col-span-12 col-span-12 mb-10  place-items-center">
                         <h1 className="text-5xl mb-5 font-bold uppercase text-left">{product.Nombre}</h1>
-                        <Carousel className={`w-2/3`} >
-                            <CarouselContent>
+                        <div ref={emblaRef} className={`2xl:w-[75%] xl:w-[75%] lg:w-[75%] md:w-[70%] sm:w-[90%] w-[100%] overflow-hidden`}
+                        >
+                            <div className="flex">
                                 {
-                                    extra.map((item) => (
-                                        <CarouselItem key={item.ID} className={``}>
-                                            <Card className={`border-none shadow-none`}>
-                                                <CardContent className={`flex items-center justify-center `}>
-                                                    {
-                                                        getExtension(item.Imagen) === 'webp' ?
-                                                            <img width={600} src={`${IMG_URL}${item.Imagen}`} alt={product.Nombre} />
-                                                            :
-                                                            <video width={600} src={`${IMG_URL}${item.Imagen}`} muted controls={false} autoPlay loop></video>
-                                                    }
-                                                </CardContent>
-                                            </Card>
-                                        </CarouselItem>
+                                    extra.map((item, idx) => (
+                                        <div className="flex-[0_0_100%] px-2 cursor-pointer" onClick={() => handleModalImg(`${IMG_URL}${item.Imagen}`)} key={idx}>
+                                            {
+                                                getExtension(item.Imagen) === 'webp' ?
+                                                    <img src={`${IMG_URL}${item.Imagen}`} alt={product.Nombre} />
+                                                    :
+                                                    <video src={`${IMG_URL}${item.Imagen}`} className="h-200" muted controls={false} autoPlay loop></video>
+                                            }
+                                        </div>
                                     ))
                                 }
-                            </CarouselContent>
-                            <CarouselPrevious className={`cursor-pointer`} />
-                            <CarouselNext className={`cursor-pointer`} />
-                        </Carousel>
+                                {/* <CarouselPrevious className={`cursor-pointer`} />
+                            <CarouselNext className={`cursor-pointer`} /> */}
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-span-4">
+                    <div className="2xl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-4 sm:col-span-12 col-span-12" >
                         <h1 className="text-3xl text-center font-semibold">Sobre el producto</h1>
                         <hr className="border my-10" />
-                        <div className="flex justify-between mb-20 text-2xl font-semibold uppercase">
+                        <div className="flex justify-between mb-10 text-2xl font-semibold uppercase">
                             <h5>Precio: </h5>
                             <h5>{formatCurrency(price ? price : product.Precio)}</h5>
                         </div>
                         {
                             description && description.length > 0 ?
-                                <div className="mb-20 text-2xl font-semibold uppercase">
+                                <div className="mb-10 text-2xl font-semibold uppercase">
                                     <h5 className="mb-5">Descripción: </h5>
                                     <h5 className="text-center font-medium">{description[0].Descripcion}</h5>
                                 </div>
@@ -143,46 +175,48 @@ export default function Product() {
                         }
                         {
                             extra.length > 0 && typeof extra[0].Color != "undefined" ?
-                                <div className="mb-20 text-2xl font-semibold uppercase">
+                                <div className="mb-10 text-2xl font-semibold uppercase">
                                     <h5 className="mb-5">Elegir Color: </h5>
-                                    <Select data={extra} labelKey="Color" valueKey="ID" />
+                                    {/* <Select data={extra} labelKey="Color" valueKey="ID" /> */}
+                                    <SelectWithImages options={extra} onSelect={scrollTo} selectedId={selectedIndex} />
+
                                 </div>
                                 :
                                 <></>
                         }
                         {
                             sizes.length > 0 && sizes ?
-                                <div className="mb-20 text-2xl font-semibold uppercase">
+                                <div className="mb-10 text-2xl font-semibold uppercase">
                                     <h5 className="mb-5">Elegir Talla: </h5>
-                                    <Select data={sizes} labelKey="Talla" valueKey="ID" />
+                                    <SelectRounded options={sizes} labelKey="Talla" valueKey="ID" selected={sizesSelected} onSelect={handleSizes} />
                                 </div>
                                 :
                                 <></>
                         }
                         {
                             type === "perfumes" ?
-                                <Select data={extra} labelKey="Tipo" valueKey="ID" onChange={handleType} />
+                                <SelectRounded options={extra} labelKey="Tipo" valueKey="ID" onSelect={handleType} selected={typeSelected} />
                                 :
                                 <></>
                         }
                         {
                             type === "calzado" ?
-                                <ButtonBlank color={"#004aad"} letterColor={"black"} >Guia de tallas</ButtonBlank>
+                                <ButtonBlank color={"#004aad"} letterColor={"black"} onClick={() => handleModalImg(sizesGuide)} >Guía de tallas</ButtonBlank>
                                 :
                                 <></>
                         }
-                        <hr className="border my-10" />
+                        <hr className="border my-7" />
                         <ButtonBlank color={"#00af4c"} letterColor={"black"} >Comprar por Whastapp</ButtonBlank>
-                        <>
-                            <hr className="border my-10" />
-                            <ButtonBlank onClick={() => isLogin ? "" : open()} color={"#ecc500"} letterColor={"black"} >Añadir al carrito</ButtonBlank>
-                        </>
+
+                        <hr className="border my-7" />
+                        <ButtonBlank onClick={() => isLogin ? "" : open()} color={"#ecc500"} letterColor={"black"} >Añadir al carrito</ButtonBlank>
                     </div>
                 </div>
             </div>
             <Footer />
             <ModalLogin close={close} isOpenModal={isOpenModal} isVisible={isVisible} openRegister={openRegister} />
             <ModalRegister close={closeRegister} isOpenModal={isOpenModalRegister} isVisible={isVisibleReigster} openLogin={open} />
+            <ModalImage image={imageModal} isOpen={isOpenModalImage} updateVal={setIsOpenModalImage} />
         </>
     )
 }
