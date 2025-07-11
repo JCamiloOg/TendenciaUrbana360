f1e5ade7b5c944ca931066240fc6b05f4fa7a3db
Camilo <osoriojuancamilo315@gmail.com>
2025-07-11 16:23:17 -0500

    feat: agrega la vista para los productos por categoria

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -6,6 +6,7 @@ import Error from "../pages/Error";
 import AuthUser from "../pages/AuthUser";
 import Profile from "../pages/Profile";
 import Product from "@/pages/Product";
+import ProductsByCategory from "@/pages/ProductsByCategory";
 
 export default function AppRoutes() {
     return (
@@ -14,7 +15,7 @@ export default function AppRoutes() {
             <Route path="/products" element={<Products />} />
             <Route path="/clients/profile" element={<Profile />} />
             <Route path="/login/success" element={<AuthUser />} />
-            <Route path="/products/:type" />
+            <Route path="/products/:type" element={<ProductsByCategory />} />
             <Route path="/products/:type/:id" element={<Product />} />
             <Route path="*" element={<Error status={404} error={"Página no encontrada."} />} />
         </Routes>
