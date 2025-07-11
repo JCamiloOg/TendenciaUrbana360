2e31e863fd8f09de2264db99facf8b34a7e1ead6
Camilo <osoriojuancamilo315@gmail.com>
2025-07-11 16:25:36 -0500

    feat: agrega la peticion para los productos por categoria

--- client/src/services/productsService.js
+++ client/src/services/productsService.js
@@ -8,7 +8,11 @@ export async function getAllProducts() {
     return await axiosInstance.get(`/products`);
 }
 
+export async function getProductsByCategory(category) {
+    return await axiosInstance.get(`/products/${category}`);
+
+}
+
 export async function getProductByID(type, id) {
     return await axiosInstance.get(`/products/${type}/${id}`)
-
 }
