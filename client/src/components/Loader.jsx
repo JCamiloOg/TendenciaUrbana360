c66f45a8efe6726eb511454ed2a334e059500f55
Camilo <osoriojuancamilo315@gmail.com>
2025-07-16 17:46:28 -0500

    style: el loader ocupa toda la pantalla sin importar el scroll

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -8,12 +8,14 @@ export default function Loader({ isVisible }) {
             // Espera la animación antes de ocultar completamente
             const timeout = setTimeout(() => setHidden(true), 500); // duración del fade
             return () => clearTimeout(timeout);
+        } else {
+            setHidden(false);
         }
     }, [isVisible]);
 
     if (hidden) return null;
     return (
-        <div className={`flex-col gap-4 w-full h-dvh absolute bg-white z-50 flex items-center justify-center transition-opacity duration-500  ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+        <div className={`flex-col gap-4 w-full h-dvh fixed inset-0 bg-white z-50 flex items-center justify-center transition-opacity duration-500  ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
             <div
                 className="w-20 h-20 border-4 border-transparent text-[#004aad] text-4xl animate-spin flex items-center justify-center border-t-[#004aad] rounded-full"
             >
