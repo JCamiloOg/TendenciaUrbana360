f64204c176b5b9ec7549cb3f4b525c7ea232ffca
Camilo <osoriojuancamilo315@gmail.com>
2025-08-07 20:15:38 -0500

    FEAT: aGREGA LA PROP de fondo del loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function Loader({ isVisible }) {
+export default function Loader({ isVisible, bg = "bg-white" }) {
     const [hidden, setHidden] = useState(false);
 
     useEffect(() => {
@@ -15,7 +15,7 @@ export default function Loader({ isVisible }) {
 
     if (hidden) return null;
     return (
-        <div className={`flex-col gap-4 w-full h-dvh fixed inset-0 bg-white z-50 flex items-center justify-center transition-opacity duration-500  ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+        <div className={`flex-col gap-4 w-full h-dvh fixed inset-0 ${bg} z-50 flex items-center justify-center transition-opacity duration-500  ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
             <div
                 className="w-20 h-20 border-4 border-transparent text-[#004aad] text-4xl animate-spin flex items-center justify-center border-t-[#004aad] rounded-full"
             >
