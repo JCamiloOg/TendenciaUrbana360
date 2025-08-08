24b62172f4b12c166aa63e4c212a215c8a39a3b3
Camilo <osoriojuancamilo315@gmail.com>
2025-08-07 20:07:57 -0500

    REFACTOR: envia como respuesta el estado y un mensaje

--- server/middlewares/verifyURL.js
+++ server/middlewares/verifyURL.js
@@ -5,7 +5,7 @@ export function verifyURL(req, res, next) {
 
     const url = req.params.typeProduct;
 
-    if (!categoriasValidas.includes(url)) return res.status(404).json('404');
+    if (!categoriasValidas.includes(url)) return res.status(404).json({ message: 'Página no encontrada.', status: 404 });
 
     next();
 }
