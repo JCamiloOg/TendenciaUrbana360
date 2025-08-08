6a92d84af5849198d8553482f0b751c9f1a9dbee
Camilo <osoriojuancamilo315@gmail.com>
2025-08-07 20:17:38 -0500

    FEAT: agrega la prop dark si quiero que el componente sea oscuro o blanco

--- client/src/components/Inputs/DatePicker.jsx
+++ client/src/components/Inputs/DatePicker.jsx
@@ -6,11 +6,11 @@ import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
 import { formatDate } from "@/utils/formatDate";
 
-export default function DatePicker({ label, text, onChange }) {
+export default function DatePicker({ label, text, onChange, dark = false }) {
     const [date, setDate] = useState(null)
     const [open, setOpen] = useState(false);
     return (
-        <div className="flex flex-col gap-3">
+        <div className={`flex flex-col gap-3 `}>
             <label htmlFor="date" className="px-1">
                 {label}
             </label>
@@ -19,14 +19,15 @@ export default function DatePicker({ label, text, onChange }) {
                     <Button
                         variant="outline"
                         id="date"
-                        className="w-48 justify-between font-normal"
+                        className={`w-48 justify-between font-normal cursor-pointer ${dark ? "bg-[#353535] hover:bg-[#4a4a4a] hover:text-white text-white border-none" : "bg-white text-black "}`}
                     >
                         {date ? formatDate(date.toLocaleDateString()) : text}
                         <FontAwesomeIcon icon={faChevronDown} />
                     </Button>
                 </PopoverTrigger>
-                <PopoverContent className="w-auto overflow-hidden p-0 z-10" align="start">
+                <PopoverContent className={`w-auto overflow-hidden p-0 z-10 ${dark ? "bg-[#353535]" : "bg-white"}`} align="start">
                     <Calendar
+                        className={`${dark ? "bg-[#353535] text-white rounded-md" : "bg-white"}`}
                         mode="single"
                         selected={date}
                         captionLayout="dropdown"
--- client/src/components/Inputs/Select.jsx
+++ client/src/components/Inputs/Select.jsx
@@ -16,9 +16,9 @@ import { useEffect, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCheck, faUpDown } from "@fortawesome/free-solid-svg-icons"
 
-export default function Select({ data, labelKey = "label", valueKey = "ID", onChange }) {
+export default function Select({ data, labelKey = "label", valueKey = "ID", onChange, error, defaultValue = "" }) {
     const [open, setOpen] = useState(false);
-    const [value, setValue] = useState("");
+    const [value, setValue] = useState(defaultValue);
 
     const selectedLabel = data.find((item) => item[valueKey].toString() === value)?.[labelKey];
 
@@ -27,44 +27,51 @@ export default function Select({ data, labelKey = "label", valueKey = "ID", onCh
     }, [value, onChange])
 
     return (
-        <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-                <Button
+        <>
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button
 
-                    role="combobox"
-                    aria-expanded={open}
-                    className="w-full justify-between cursor-pointer font-bold !bg-[#002960] text-lg"
-                >
-                    {value
-                        ? selectedLabel
-                        : `Elegir ${labelKey}...`}
-                    <FontAwesomeIcon icon={faUpDown} className="opacity-50 " />
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className=" p-0 !bg-[#002960]">
-                <Command className={`!bg-[#002960]`}>
-                    <CommandInput placeholder={`Buscar ${labelKey}...`} className="placeholder:text-white flex w-full rounded-md bg-transparent py-3 text-sm outline-hidden disabled:cursor-not-allowed disabled:opacity-50 h-9 !text-white" />
-                    <CommandList>
-                        <CommandEmpty className="py-6 text-center text-sm text-white font-semibold">No se encontró {labelKey}</CommandEmpty>
-                        <CommandGroup>
-                            {data.map((item) => (
-                                <CommandItem
-                                    key={item[valueKey]}
-                                    value={item[valueKey].toString()}
-                                    onSelect={(currentValue) => {
-                                        setValue(currentValue == value ? "" : currentValue)
-                                        setOpen(false)
-                                    }}
-                                    className={`data-[selected=true]:bg-[#00132c] data-[selected=true]:text-white data-[selected=false]:text-white [&_svg:not([class*='text-'])]:text-muted-foreground relative flex items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 cursor-pointer w-full justify-between font-semibold`}
-                                >
-                                    {item[labelKey]}
-                                    <FontAwesomeIcon icon={faCheck} className={`text-white ${value == item[valueKey].toString() ? "opacity-100" : "opacity-0"}`} />
-                                </CommandItem>
-                            ))}
-                        </CommandGroup>
-                    </CommandList>
-                </Command>
-            </PopoverContent>
-        </Popover>
+                        role="combobox"
+                        aria-expanded={open}
+                        className={`w-full justify-between border border-transparent  cursor-pointer !bg-[#212121] text-md ${error ? "border-red-500 text-red-500" : ""}`}
+                    >
+                        {value
+                            ? selectedLabel
+                            : `Elegir ${labelKey}...`}
+                        <FontAwesomeIcon icon={faUpDown} className="opacity-50 " />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className=" p-0 !bg-[#353535]">
+                    <Command className={`!bg-[#353535]`}>
+                        <CommandInput placeholder={`Buscar ${labelKey}...`} className="placeholder:text-white flex w-full rounded-md bg-transparent py-3 text-sm outline-hidden disabled:cursor-not-allowed disabled:opacity-50 h-9 !text-white" />
+                        <CommandList>
+                            <CommandEmpty className="py-6 text-center text-sm text-white font-semibold">No se encontró {labelKey}</CommandEmpty>
+                            <CommandGroup>
+                                {data.map((item) => (
+                                    <CommandItem
+                                        key={item[valueKey]}
+                                        value={item[valueKey].toString()}
+                                        onSelect={(currentValue) => {
+                                            setValue(currentValue == value ? "" : currentValue)
+                                            setOpen(false)
+                                        }}
+                                        className={`data-[selected=true]:bg-[#4a4a4a] data-[selected=true]:text-white data-[selected=false]:text-white [&_svg:not([class*='text-'])]:text-muted-foreground relative flex items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 cursor-pointer w-full justify-between font-semibold`}
+                                    >
+                                        {item[labelKey]}
+                                        <FontAwesomeIcon icon={faCheck} className={`text-white ${value == item[valueKey].toString() ? "opacity-100" : "opacity-0"}`} />
+                                    </CommandItem>
+                                ))}
+                            </CommandGroup>
+                        </CommandList>
+                    </Command>
+                </PopoverContent>
+            </Popover>
+            {
+                error && (
+                    <span className="text-red-500 mt-6">{error}</span>
+                )
+            }
+        </>
     )
 }
