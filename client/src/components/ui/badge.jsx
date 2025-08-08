c6177500e65d4c57a4630a73186b122d94bb545a
Camilo <osoriojuancamilo315@gmail.com>
2025-08-07 20:15:17 -0500

    FEAT: nuevos componentes de sahdCN

--- client/src/components/ui/badge.jsx
+++ client/src/components/ui/badge.jsx
@@ -1,15 +1,18 @@
 export default function Badge({ type, children }) {
     const options = {
-        default: "bg-gray-800 text-white",
-        primary: "bg-indigo-500 text-white",
-        success: "bg-green-500 text-white",
-        danger: "bg-red-500 text-white",
-        warning: "bg-yellow-500 text-black",
-        info: "bg-blue-500 text-white",
+        default: "bg-gray-200 hover:bg-gray-300 text-gray-600",
+        success: "bg-green-200 hover:bg-green-300 text-green-600",
+        danger: "bg-red-200 hover:bg-red-300 text-red-600",
+        warning: "bg-yellow-200 hover:bg-yellow-300 text-yellow-600",
+        info: "bg-blue-200 hover:bg-blue-300 text-blue-600",
     }
     const bg = options[type] || options.default;
 
     return (
-        <span className={`${bg} rounded-full px-3 py-2 w-35 text-center text-xs uppercase font-bold`}>{children}</span>
+        <span className={`inline-flex items-center m-2 px-3 py-1 rounded-full text-sm font-semibold ${bg}`}>
+            <span className="ml-1">
+                {children}
+            </span>
+        </span>
     )
 }
