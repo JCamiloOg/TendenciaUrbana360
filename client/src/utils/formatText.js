14e9ccd1d03438d11d0eaa02a367749807c1fd0a
Camilo <osoriojuancamilo315@gmail.com>
2025-08-07 20:09:36 -0500

    FEAT: agrega una funcion para obtener la primera letra de un texto en mayuscula

--- client/src/utils/formatText.js
+++ client/src/utils/formatText.js
@@ -1,3 +1,7 @@
 export function textCapitalize(txt) {
     return txt.charAt(0).toUpperCase() + txt.slice(1);
 }
+
+export function getInitialLetter(txt) {
+    return txt.charAt(0).toUpperCase()
+}
