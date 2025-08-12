6649a41a7780ad461117a19642ae571fb2822011
Camilo <osoriojuancamilo315@gmail.com>
2025-08-11 20:16:34 -0500

    STYLE: Más parecido a la card de los productos

--- client/src/components/ui/SkeletonCard.jsx
+++ client/src/components/ui/SkeletonCard.jsx
@@ -2,11 +2,11 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export function SkeletonCard() {
     return (
-        <div className="bg-white p-5 rounded-md">
-            <Skeleton className="h-70 rounded-xl w-full mb-3 bg-gray-500/30" />
-            <div className="space-y-2">
+        <div className="bg-white rounded-xl">
+            <Skeleton className="h-70 rounded-b-none rounded-t-xl   w-full mb-3 bg-gray-500/30" />
+            <div className="space-y-2 p-5">
                 <Skeleton className="h-4 w-[250px] bg-gray-500/30" />
-                <Skeleton className="h-4 w-[200px] bg-gray-500/30" />
+                <Skeleton className="h-4 w-[200px] bg-gray-500/30 mt-4" />
             </div>
         </div>
     )
