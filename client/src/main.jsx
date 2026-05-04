f992e1964415036062abbad3be3422b6c1000315
Camilo <166083316+JCamiloOg@users.noreply.github.com>
2026-05-03 19:40:46 -0500

    FIX: StricMode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"
 import App from './App.jsx'
 import ScrollToTop from "./hooks/useScrollTop.js";
+import { StrictMode } from "react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ScrollToTop />
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>,
+  </StrictMode>
 )
